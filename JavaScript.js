85998f58db3cab23dbd46b2c4aeaeffd75cbf65c
SkerLeon <sker870812@gmail.com>
2023-12-13 20:56:03 +0800

    2023.12.13 dog show 動態效果

--- JavaScript.js
+++ JavaScript.js
@@ -245,10 +245,6 @@ function bannerPush(){
     }
 }
 
-
-
-
-
 function linkControlBanner(e){
     //在按下link的點點時
     //清除計時器並新增 就會有重置的效果
@@ -305,3 +301,223 @@ if(window.innerWidth>768){
     };
 }
 
+
+// DOG SHOW動態效果
+let filterItemDog = document.getElementsByClassName("filter-item-dog")
+
+// DOG SHOW 圖片
+let dogPicImg01 = document.getElementById("dog-pic-img-01")
+let dogPicImg02 = document.getElementById("dog-pic-img-02")
+let dogPicImg03 = document.getElementById("dog-pic-img-03")
+let dogPicImg04 = document.getElementById("dog-pic-img-04")
+let dogPicImg05 = document.getElementById("dog-pic-img-05")
+let dogPicImg06 = document.getElementById("dog-pic-img-06")
+let dogPicImg07 = document.getElementById("dog-pic-img-07")
+let dogPicImg08 = document.getElementById("dog-pic-img-08")
+let dogPicImg09 = document.getElementById("dog-pic-img-09")
+let dogPicImg10 = document.getElementById("dog-pic-img-10")
+let dogPicImg11 = document.getElementById("dog-pic-img-11")
+let dogPicImg12 = document.getElementById("dog-pic-img-12")
+
+//DOG SHOW 稱號
+let itemDogNickname01 = document.getElementById("item-dog-nickname01")
+let itemDogNickname02 = document.getElementById("item-dog-nickname02")
+let itemDogNickname03 = document.getElementById("item-dog-nickname03")
+let itemDogNickname04 = document.getElementById("item-dog-nickname04")
+let itemDogNickname05 = document.getElementById("item-dog-nickname05")
+let itemDogNickname06 = document.getElementById("item-dog-nickname06")
+let itemDogNickname07 = document.getElementById("item-dog-nickname07")
+let itemDogNickname08 = document.getElementById("item-dog-nickname08")
+let itemDogNickname09 = document.getElementById("item-dog-nickname09")
+let itemDogNickname10 = document.getElementById("item-dog-nickname10")
+let itemDogNickname11 = document.getElementById("item-dog-nickname11")
+let itemDogNickname12 = document.getElementById("item-dog-nickname12")
+
+//DOG SHOW內容
+let itemDogIntroduce01 = document.getElementById("item-dog-introduce01")
+let itemDogIntroduce02 = document.getElementById("item-dog-introduce02")
+let itemDogIntroduce03 = document.getElementById("item-dog-introduce03")
+let itemDogIntroduce04 = document.getElementById("item-dog-introduce04")
+let itemDogIntroduce05 = document.getElementById("item-dog-introduce05")
+let itemDogIntroduce06 = document.getElementById("item-dog-introduce06")
+let itemDogIntroduce07 = document.getElementById("item-dog-introduce07")
+let itemDogIntroduce08 = document.getElementById("item-dog-introduce08")
+let itemDogIntroduce09 = document.getElementById("item-dog-introduce09")
+let itemDogIntroduce10 = document.getElementById("item-dog-introduce10")
+let itemDogIntroduce11 = document.getElementById("item-dog-introduce11")
+let itemDogIntroduce12 = document.getElementById("item-dog-introduce12")
+
+//DOG SHOW按鈕
+let filterItemH201 = document.getElementById("filter-item-h201")
+let filterItemH202 = document.getElementById("filter-item-h202")
+let filterItemH203 = document.getElementById("filter-item-h203")
+let filterItemH204 = document.getElementById("filter-item-h204")
+let filterItemH205 = document.getElementById("filter-item-h205")
+let filterItemH206 = document.getElementById("filter-item-h206")
+let filterItemH207 = document.getElementById("filter-item-h207")
+let filterItemH208 = document.getElementById("filter-item-h208")
+let filterItemH209 = document.getElementById("filter-item-h209")
+let filterItemH210 = document.getElementById("filter-item-h210")
+let filterItemH211 = document.getElementById("filter-item-h211")
+let filterItemH212 = document.getElementById("filter-item-h212")
+
+//滑鼠進入
+function DOGSHOWSelectionEffectIn (e){
+    if(e.target.id == "item-dog-01"){
+        dogPicImg01.style.transform = "scale(1.1)"
+        itemDogNickname01.style.color = "#000"
+        itemDogIntroduce01.style.color = "#000"
+        filterItemH201.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-02"){
+        dogPicImg02.style.transform = "scale(2.1)"
+        itemDogNickname02.style.color = "#000"
+        itemDogIntroduce02.style.color = "#000"
+        filterItemH202.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-03"){
+        dogPicImg03.style.transform = "scale(1.5)"
+        itemDogNickname03.style.color = "#000"
+        itemDogIntroduce03.style.color = "#000"
+        filterItemH203.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-04"){
+        dogPicImg04.style.transform = "scale(2.1)"
+        itemDogNickname04.style.color = "#000"
+        itemDogIntroduce04.style.color = "#000"
+        filterItemH204.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-05"){
+        dogPicImg05.style.transform = "scale(1.1)"
+        itemDogNickname05.style.color = "#000"
+        itemDogIntroduce05.style.color = "#000"
+        filterItemH205.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-06"){
+        dogPicImg06.style.transform = "scale(1.9)"
+        itemDogNickname06.style.color = "#000"
+        itemDogIntroduce06.style.color = "#000"
+        filterItemH206.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-07"){
+        dogPicImg07.style.transform = "scale(2.3)"
+        itemDogNickname07.style.color = "#000"
+        itemDogIntroduce07.style.color = "#000"
+        filterItemH207.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-08"){
+        dogPicImg08.style.transform = "scale(1.1)"
+        itemDogNickname08.style.color = "#000"
+        itemDogIntroduce08.style.color = "#000"
+        filterItemH208.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-09"){
+        dogPicImg09.style.transform = "scale(1.1)"
+        itemDogNickname09.style.color = "#000"
+        itemDogIntroduce09.style.color = "#000"
+        filterItemH209.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-10"){
+        dogPicImg10.style.transform = "scale(1.1)"
+        itemDogNickname10.style.color = "#000"
+        itemDogIntroduce10.style.color = "#000"
+        filterItemH210.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-11"){
+        dogPicImg11.style.transform = "scale(1.1)"
+        itemDogNickname11.style.color = "#000"
+        itemDogIntroduce11.style.color = "#000"
+        filterItemH211.style.backgroundColor ="#F4B410"
+    }
+    if(e.target.id == "item-dog-12"){
+        dogPicImg12.style.transform = "scale(1.3)"
+        itemDogNickname12.style.color = "#000"
+        itemDogIntroduce12.style.color = "#000"
+        filterItemH212.style.backgroundColor ="#F4B410"
+    }
+    
+}
+
+//滑鼠移出
+function DOGSHOWSelectionEffectOut (e){
+    if(e.target.id == "item-dog-01"){
+        dogPicImg01.style.transform = "scale(1)"
+        itemDogNickname01.style.color = "#474143"
+        itemDogIntroduce01.style.color = "#474143"
+        filterItemH201.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-02"){
+        dogPicImg02.style.transform = "scale(2)"
+        itemDogNickname02.style.color = "#474143"
+        itemDogIntroduce02.style.color = "#474143"
+        filterItemH202.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-03"){
+        dogPicImg03.style.transform = "scale(1.4)"
+        itemDogNickname03.style.color = "#474143"
+        itemDogIntroduce03.style.color = "#474143"
+        filterItemH203.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-04"){
+        dogPicImg04.style.transform = "scale(2)"
+        itemDogNickname04.style.color = "#474143"
+        itemDogIntroduce04.style.color = "#474143"
+        filterItemH204.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-05"){
+        dogPicImg05.style.transform = "scale(1)"
+        itemDogNickname05.style.color = "#474143"
+        itemDogIntroduce05.style.color = "#474143"
+        filterItemH205.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-06"){
+        dogPicImg06.style.transform = "scale(1.8)"
+        itemDogNickname06.style.color = "#474143"
+        itemDogIntroduce06.style.color = "#474143"
+        filterItemH206.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-07"){
+        dogPicImg07.style.transform = "scale(2.2)"
+        itemDogNickname07.style.color = "#474143"
+        itemDogIntroduce07.style.color = "#474143"
+        filterItemH207.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-08"){
+        dogPicImg08.style.transform = "scale(1)"
+        itemDogNickname08.style.color = "#474143"
+        itemDogIntroduce08.style.color = "#474143"
+        filterItemH208.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-09"){
+        dogPicImg09.style.transform = "scale(1)"
+        itemDogNickname09.style.color = "#474143"
+        itemDogIntroduce09.style.color = "#474143"
+        filterItemH209.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-10"){
+        dogPicImg10.style.transform = "scale(1)"
+        itemDogNickname10.style.color = "#474143"
+        itemDogIntroduce10.style.color = "#474143"
+        filterItemH210.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-11"){
+        dogPicImg11.style.transform = "scale(1)"
+        itemDogNickname11.style.color = "#474143"
+        itemDogIntroduce11.style.color = "#474143"
+        filterItemH211.style.backgroundColor ="#3A3967"
+    }
+    if(e.target.id == "item-dog-12"){
+        dogPicImg12.style.transform = "scale(1.2)"
+        itemDogNickname12.style.color = "#474143"
+        itemDogIntroduce12.style.color = "#474143"
+        filterItemH212.style.backgroundColor ="#3A3967"
+    }
+}
+
+if(window.innerWidth>768){
+    for (let i = 0; i < filterItemDog.length; i++) {
+        filterItemDog[i].addEventListener("mouseenter", DOGSHOWSelectionEffectIn);
+
+        filterItemDog[i].addEventListener("mouseleave", DOGSHOWSelectionEffectOut);
+    };
+}
